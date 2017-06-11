96db8e081fbba701dec4c1724da9e1e47a94ea1b
Diane Ela <dela1@my.westga.edu>
2017-06-11 17:04:09 -0400

    Created test class to test the getSticksLeft method in the Pile class

---
+++ nim01/src/edu/westga/cs6910/nim/testing/PileGetSticksLeft.java
@@ -0,0 +1,14 @@
+package edu.westga.cs6910.nim.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PileGetSticksLeft {
+
+	@Test
+	public void testGetSticksLeft() {
+		fail("Not yet implemented");
+	}
+
+}
