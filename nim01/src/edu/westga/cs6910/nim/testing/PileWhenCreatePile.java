a0c603ef1502aee48a4195c7431e57e8ac5309da
Diane Ela <dela1@my.westga.edu>
2017-06-11 16:26:13 -0400

    New Junit test class for Pile class

---
+++ nim01/src/edu/westga/cs6910/nim/testing/PileWhenCreatePile.java
@@ -0,0 +1,19 @@
+package edu.westga.cs6910.nim.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PileWhenCreatePile {
+
+	@Test
+	public void testRemoveSticks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSticksLeft() {
+		fail("Not yet implemented");
+	}
+
+}
