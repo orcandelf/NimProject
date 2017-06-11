132f49e9adef31fb4a03c496eead40de53b16c75
Diane Ela <dela1@my.westga.edu>
2017-06-11 17:03:46 -0400

    Added Javadoc comments

--- nim01/src/edu/westga/cs6910/nim/testing/PileWhenCreatePile.java
+++ nim01/src/edu/westga/cs6910/nim/testing/PileWhenCreatePile.java
@@ -14,18 +14,27 @@ import edu.westga.cs6910.nim.model.Pile;
  */
 public class PileWhenCreatePile {
 
+	/**
+	 * Test to verify 1 stick in pile
+	 */
 	@Test
 	public void testShouldProducePileWith1Stick() {
 		Pile oneStickPile = new Pile(1);
 		assertEquals("Pile size: 1", oneStickPile.toString());
 	}
 
+	/**
+	 * Test to verify 10 sticks in pile
+	 */
 	@Test
 	public void testShouldProducePileWith10Sticks() {
 		Pile tenStickPile = new Pile(10);
 		assertEquals("Pile size: 10", tenStickPile.toString());
 	}
 	
+	/**
+	 * Test to verify 100 sticks in pile
+	 */
 	@Test
 	public void testShouldProducePileWith100Sticks() {
 		Pile oneHundredStickPile = new Pile(100);
