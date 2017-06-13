96c23265a89da83eb2f059444ed28c5bd8b92fcc
Diane Ela <dela1@my.westga.edu>
2017-06-13 17:06:58 -0400

    Added final tests for number of sticks and Javadoc comments

--- nim01/src/edu/westga/cs6910/nim/testing/GameWhenStartingGame.java
+++ nim01/src/edu/westga/cs6910/nim/testing/GameWhenStartingGame.java
@@ -8,6 +8,7 @@ import edu.westga.cs6910.nim.model.HumanPlayer;
 import edu.westga.cs6910.nim.model.ComputerPlayer;
 
 /**
+ * Test class for the Game class
  * 
  * @author Diane Ela
  * @version Jun 13, 2017
@@ -16,7 +17,7 @@ import edu.westga.cs6910.nim.model.ComputerPlayer;
 public class GameWhenStartingGame {
 
 	/**
-	 * 
+	 * Tests the creation of the human player in the Game constructor
 	 */
 	@Test
 	public void testGameWithHumanNamedHuman() {
@@ -24,15 +25,31 @@ public class GameWhenStartingGame {
 		assertEquals(newGame.getHumanPlayer().getName(), "Human");
 	}
 	
+	/**
+	 * Tests the creation of the computer player in the Game constructor
+	 */
 	@Test
 	public void testGameWithComputerName() {
 		Game newGame = new Game(new HumanPlayer("Human"), new ComputerPlayer());
 		assertEquals(newGame.getComputerPlayer().getName(), "Simple computer");
 	}
 	
+	
+	/**
+	 * Tests the default number of sticks in the pile when starting a new game
+	 */
 	@Test
 	public void testGameStartingPile() {
 		Game newGame = new Game(new HumanPlayer("Human"), new ComputerPlayer());
 		assertEquals(newGame.getPile().toString(), "Pile size: 7");
+	}
+	
+	/**
+	 * Tests the number of sticks left when starting a new game
+	 */
+	@Test
+	public void testGameStickLeftStartingPile() {
+		Game newGame = new Game(new HumanPlayer("Human"), new ComputerPlayer());
+		assertEquals(newGame.getSticksLeft(), 7);
 	}
 }
