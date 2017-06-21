a75848c010a15aeecb890505ecfa4a6437996234
Diane Ela <dela1@my.westga.edu>
2017-06-20 21:22:44 -0400

    Updated random strategy to use Math.rand to determine random number inclusive

---
+++ nim01/src/edu/westga/cs6910/nim/model/strategy/RandomStrategy.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+package edu.westga.cs6910.nim.model.strategy;
+
+import edu.westga.cs6910.nim.model.Game;
+
+/**
+ * @author Diane Ela
+ * @version Jun 18, 2017
+ *
+ */
+public class RandomStrategy implements NumberOfSticksStrategy {
+
+	/* (non-Javadoc)
+	 * @see edu.westga.cs6910.nim.model.strategy.NumberOfSticksStrategy#howManySticks(int)
+	 */
+	@Override
+	public int howManySticks(int pileSize) {
+		int sticksToTake = 0;
+		if (pileSize > 0) {
+			int min = 1;
+			int max = Math.min(pileSize - 1, Game.MAX_STICKS_PER_TURN);
+			sticksToTake = (int) (min + (Math.random() * (max - min)));
+		}
+		return sticksToTake;
+	}
+
+}
