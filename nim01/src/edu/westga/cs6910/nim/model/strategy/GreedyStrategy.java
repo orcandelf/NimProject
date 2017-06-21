3539e8fe45a17a384936e0419f21ac1a4b5a27f1
Diane Ela <dela1@my.westga.edu>
2017-06-20 21:25:13 -0400

    Created greedy strategy with max sticks per turn from Game

---
+++ nim01/src/edu/westga/cs6910/nim/model/strategy/GreedyStrategy.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+package edu.westga.cs6910.nim.model.strategy;
+
+import edu.westga.cs6910.nim.model.Game;
+
+/**
+ *Class for the cautious strategy where computer takes 1 stick only
+ * 
+ * @author Diane Ela
+ * @version Jun 18, 2017
+ *
+ */
+public class GreedyStrategy implements NumberOfSticksStrategy {
+
+	/**
+	 * Returns 1 stick to take always
+	 */
+	@Override
+	public int howManySticks(int pileSize) {
+		int sticksToTake = 0;
+		if (pileSize > 0) {
+			sticksToTake = Game.MAX_STICKS_PER_TURN;
+		}
+		return sticksToTake;
+	}
+}
